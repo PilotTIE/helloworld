ed1b0d9517554300b0f867509582bb60395a6752
Vasyan <Vasyan@DESKTOP-1M9D9FV>
2024-03-03 15:05:47 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello! Who are you?");
+string user;
+user = Console.ReadLine();
+Console.WriteLine($"Hello, {user}");
+Console.WriteLine("Thanks to my Dad for helping me write this!");
+Console.WriteLine("Best Regards, author.");
