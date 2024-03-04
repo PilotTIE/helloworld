a703181fc2e02acec6d440306a50fe20728d47be
Vasyan <vastyn85@gmail.com>
2024-03-04 20:43:05 +0900

    feat: ourSuperCalc

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using helloworld.classes;
+
 Console.WriteLine("Hello! Who are you?");
+
 string user;
 user = Console.ReadLine();
+
 Console.WriteLine($"Hello, {user}");
 Console.WriteLine("Thanks to my Dad for helping me write this!");
 Console.WriteLine("Best Regards, author.");
+
+
+MathCalc ourSuperCalc = new MathCalc();
+int result = ourSuperCalc.add(9, 10);
+Console.WriteLine($"Performing operation 'ourSuperCalc.add...' {result}");
+Console.WriteLine("19 is supposed to appear above. If it doesn't, then something is wrong.");
---
+++ classes/MathCalc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld.classes
+{
+    internal class MathCalc
+    {
+        public int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    
+    }
+}
+
